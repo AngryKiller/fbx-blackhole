9d1407b15ce1caf73596159ac8b0b6bf4e9678de
Steve Labus <angrykiller@icloud.com>
2021-08-28 03:17:55 +0200

    Added configurable watched folders

--- app.js
+++ app.js
@@ -4,19 +4,24 @@ const path = require('path');
 const os = require('os');
 const FormData = require('form-data');
 const { FreeboxRegister, Freebox } = require("freebox");
-
+let conf;
 const directory = ('watched');
 
-/* empty folder before starting */
-fs.readdir(directory, (err, files) => {
-    if (err) throw err;
-    for (const file of files) {
-        fs.unlink(path.join(directory, file), err => {
-            if (err) throw err;
-        });
-    }
-});
+/* create a config file based on the example at first start */
+if(!fs.existsSync("./config.json")){
+    fs.copyFile('config.example.json', 'config.json', function (err){
+        if (err) throw err;
+        console.log("Creating default config.json file. You should probably take a look at it to set your watched directories.");
+        conf = require('./config.json');
+        initFolders();
+    })
+}else{
+    conf = require('./config.json');
+    initFolders();
+}
 
+
+/* if the fbx auth info file isn't found, begin the pairing process */
 if(!fs.existsSync("./fbx.json")){
     async function main() {
         const freeboxRegister = new FreeboxRegister({
@@ -26,13 +31,11 @@ if(!fs.existsSync("./fbx.json")){
             device_name: os.hostname(),
         });
 
-        // Obtaining an app_token & everything you need
-        // following the guide at https://dev.freebox.fr/sdk/os/login/
         const access = await freeboxRegister.register();
         const fbxJson = JSON.stringify(access);
         fs.writeFile("fbx.json", fbxJson, 'utf8', function (err) {
             if (err) {
-                console.log("An error occured while writing JSON Object to File.");
+                console.log("An error occured while writing JSON object to file.");
                 return console.log(err);
             }
             console.log("JSON file containing Freebox authentication informations has been saved.");
@@ -40,7 +43,7 @@ if(!fs.existsSync("./fbx.json")){
     }
     main().catch(err => console.error(err));
 
-}else {
+}else{
     const fbxInfo = require('./fbx.json');
     async function main() {
         const freebox = new Freebox({
@@ -56,39 +59,40 @@ if(!fs.existsSync("./fbx.json")){
         // https://dev.freebox.fr/sdk/os/login/
         await freebox.login();
 
-        chokidar.watch('./watched').on('add', (event, dpath) => {
-            console.log("New file detected: "+event);
-            var fileName = path.basename('/' + event);
-            if (path.extname(fileName) !== ".torrent") {
-                console.log(`Invalid file detected: "${fileName}". Deleting it from the watched folder!`);
-                fs.unlink(path.join(directory, fileName), err => {
-                    if (err) throw err;
-                })
-            } else {
-                const formData = new FormData();
-                let downDir = '/Freebox/Films';
-                let buff = new Buffer(downDir);
-                let b64Dir = buff.toString('base64');
-                formData.append('download_dir', b64Dir);
-                formData.append('download_file', fs.createReadStream(path.join(directory, fileName)));
-                freebox.request({
-                    method: 'post',
-                    url: 'downloads/add',
-                    data: formData,
-                    headers: formData.getHeaders()
-                }).then(function(response) {
-                    console.log(response.data.success);
-                    if(response.data.success === true){
-                        console.log("Download added successfully to the Freebox at this path: "+downDir);
-                        fs.unlink(path.join(directory, fileName), err => {
-                            if (err) throw err;
-                        })
-                    }else{
-                        console.error("An error occured while adding the torrent to the Freebox: "+response.data.error_code+" ("+response.data.msg+")");
-                    }
-                }).catch(err => console.error(err));
+        conf.forEach(function(folder){
+            chokidar.watch(folder.watchedDir).on('add', (event, dpath) => {
+                console.log("New file detected: "+event);
+                let fileName = path.basename('/' + event);
+                if (path.extname(fileName) !== ".torrent") {
+                    console.log(`Invalid file detected: "${fileName}". Deleting it from the watched folder!`);
+                    fs.unlink(path.join(folder.watchedDir, fileName), err => {
+                        if (err) throw err;
+                    })
+                } else {
+                    const formData = new FormData();
+                    let downDir = folder.targetDir;
+                    let buff = new Buffer(downDir);
+                    let b64Dir = buff.toString('base64');
+                    formData.append('download_dir', b64Dir);
+                    formData.append('download_file', fs.createReadStream(path.join(folder.watchedDir, fileName)));
+                    freebox.request({
+                        method: 'post',
+                        url: 'downloads/add',
+                        data: formData,
+                        headers: formData.getHeaders()
+                    }).then(function(response) {
+                        if(response.data.success === true){
+                            console.log("Download added successfully to the Freebox at this path: "+downDir);
+                            fs.unlink(path.join(folder.watchedDir, fileName), err => {
+                                if (err) throw err;
+                            })
+                        }else{
+                            console.error("An error occured while adding the torrent to the Freebox: "+response.data.error_code+" ("+response.data.msg+")");
+                        }
+                    }).catch(err => console.error(err));
 
-            }
+                }
+            });
         });
 
         // Close the current session
@@ -98,3 +102,23 @@ if(!fs.existsSync("./fbx.json")){
     main().catch(err => console.error(err));
 }
 
+function initFolders(){
+    /* empty watched folders before starting */
+    conf.forEach(function (folder){
+        if(!fs.existsSync(folder.watchedDir)){
+            fs.mkdir(path.join(__dirname, folder.watchedDir), {recursive: true}, function(err){
+                if (err) throw err;
+                console.log("Watched directory '"+folder.watchedDir+"' did not exist, created it for you.")
+            })
+        }else {
+            fs.readdir(folder.watchedDir, (err, files) => {
+                if (err) throw err;
+                for (const file of files) {
+                    fs.unlink(path.join(folder.watchedDir, file), err => {
+                        if (err) throw err;
+                    });
+                }
+            });
+        }
+    });
+}
